8105e2057a7c1c33cc8463a7d7ca582d534953a9
vivekiet22 <vivekmaddheshiya205@gmail.com>
2022-02-19 00:07:03 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      Vivek's todo list
+      <p>this is my first react.js based project</p>
+    </div>
+  );
+};
